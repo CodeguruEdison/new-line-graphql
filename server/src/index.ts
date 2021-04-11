bc0771caa4ddd6449b98e1ab62acf2358f1ea95c
Ayaskanta Samal <silu.samal@gmail.com>
2021-04-11 09:56:40 -0400

    added query

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,25 +2,26 @@
 import express from "express";
 import { listingsData } from './listings';
 import bodyParser   from 'body-parser';
-
-const app = express();
-app.use(express.json())
-//app.use(bodyParser.json());
-const port = 9000;
-app.get('/', (req, res) => res.send('hello world1'));
-app.get('/listings',(_req,res)=>{
-    res.send(listingsData)
-});
-app.post('/delete-listing',(req,res)=>{
-    const id:string = req.body.id;
-    for (let i = 0; i < listingsData.length; i++) {
-        if (listingsData[i].id === id) {
-          return res.send(listingsData.splice(i, 1)[0]);
-        }
+import { ApolloServer } from "apollo-server-express";
+import { GraphQLSchema, GraphQLObjectType } from "graphql";
+const query = new GraphQLObjectType({
+    name: "Query",
+    fields: {
+        hello: {}
       }
-    return res.send("failed to deleted listing");
-});
-//
+  });
+  
+  const mutation = new GraphQLObjectType({
+    name: "Mutation"
+  });
+export const schema = new GraphQLSchema({
+    query,
+    mutation
+ });
+const app = express();
+const port = 9000;
+const server = new ApolloServer();
+server.applyMiddleware({ app, path: "/api" });
 app.listen(port);
 console.log(`[app] : http://localhost:${port}`);
 
